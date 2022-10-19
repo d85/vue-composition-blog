4414f4781d4bbf370a15e070620fac34a5851527
Dan Carlier <dan.carlier@gmail.com>
2022-10-19 14:26:18 +1100

    Add post detail route and create getPost Composable

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DetailsView from '../views/DetailsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/posts/:id',
+      name: 'details',
+      component: DetailsView,
+      props: true // enables accessing the route parameter as a prop
     }
   ]
 })
