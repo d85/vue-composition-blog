06f8c572d5c5e9789bd29ac0b3435eba103c34c5
Dan Carlier <dan.carlier@gmail.com>
2022-10-19 13:15:59 +1100

    Initial commit, display list of blogs

---
+++ src/components/PostList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+      <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from 'vue'
+import SinglePost from './SinglePost.vue'
+
+export default {
+    props: ["posts"],
+    components: { SinglePost },
+    setup(props) {
+      onMounted(() => {
+        console.log('component mounted')
+      })
+      onUnmounted(() => {
+        console.log('component unmounted')
+      })
+      onUpdated(() => {
+        console.log('component updated')
+      })
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
+  </div>
+</template>
+
+<script>
+import PostList from '@/components/PostList.vue'
+import { ref } from 'vue'
+
+export default {
+    name: "Home",
+    components: { PostList },
+    setup() {
+      const posts = ref([])
+      const error = ref(null)
+
+      const load = async () => {
+        try {
+          let data = await fetch('http://localhost:3000/posts')
+          if (!data.ok) {
+            throw Error('no data available')
+          }
+          posts.value = await data.json()
+        } catch (err) {
+            error.value = err.message
+            console.log(error.value)
+        }
+      }
+
+      load()
+
+      return { posts, error }
+    },
+    components: { PostList }
+}
+</script>
+
+<style scoped>
+  .home {
+    width: 100%;
+    text-align: center;
+  }
+</style>
